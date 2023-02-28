71b0e13e21316a2763cf02bc3586a4d59c684bf3
whmsft <whmsft@outlook.com>
2023-02-28 11:07:57 +0530

    added bullet afterall... A lot of work left

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,13 +1,19 @@
 extends Node2D
 export var INTERNAL = {"width": 40, "height": 80}
+var frame = 0
 var Mouse = Vector2()
+var bullets = []
 
 func _ready():
 	$Player.position = Vector2(16, 62)
+	$Player.play()
+	$Bullet.hide()
 
 func _process(_delta):
-	var Mouse = get_global_mouse_position()
+	frame += 1
+	Mouse = get_global_mouse_position()
 	update_player()
+	update_bullet()
 
 func update_player():
 	var old_position = $Player.position 
@@ -18,3 +24,13 @@ func update_player():
 	$Player.flip_h = bool($Player.position.x-old_position.x < 0)
 	if $Player.position.x < -4 : $Player.position.x = 48
 	elif $Player.position.x > 48: $Player.position.x = -4
+	
+func update_bullet():
+	if (Input.is_action_pressed("key_space") or $btn_fire.pressed) and (frame%10 == 0):
+		bullets.append($Bullet.duplicate())
+		bullets[-1].position = Vector2($Player.position.x, $Player.position.y)
+		bullets[-1].show()
+		add_child(bullets[-1])
+	for n in bullets:
+		if n.position.y < 8: bullets.erase(n)
+		n.position = Vector2(n.position.x, n.position.y-2.5)
