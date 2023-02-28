0829841571bdd797db447b85f11c57aa326f3d72
whmsft <whmsft@outlook.com>
2023-02-28 17:00:39 +0530

    A lot of work.. changes on diff.txt

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,38 +1,59 @@
+#####################
+## Name: vechten   ##
+## Author: @whmsft ##
+## version: v0b1   ##
+## commit: 11      ##
+#####################
+
 extends Node2D
-var frame = 0
-var Mouse = Vector2()
+var FPS = 0
+var SCREEN = Vector2(0,0)
+var MOUSE = Vector2()
 var bullets = []
 
+var timer = 0
+var timer_limit = 0.2
+
 func _ready():
-	$Player.position = Vector2(16, 62)
+	SCREEN.x = get_viewport().get_visible_rect().size.x
+	SCREEN.y = get_viewport().get_visible_rect().size.y
+	$btn_fire.set_global_position(Vector2(SCREEN.x -9, SCREEN.y -9))
+	$btn_left.set_global_position(Vector2(1, SCREEN.y -9))
+	$btn_right.set_global_position(Vector2(10, SCREEN.y -9))
+	$Player.position = Vector2(16, SCREEN.y -18)
 	$Player.play()
 	$Bullet.hide()
 
 func _process(_delta):
-	frame += 1
-	Mouse = get_global_mouse_position()
-	update_player()
-	update_bullet()
+	FPS = Engine.get_frames_per_second()
+	$Label.text = "FPS:"+str(FPS)
+	MOUSE = get_global_mouse_position()
+	update_player(_delta)
+	update_bullet(_delta)
 
-func update_player():
+func update_player(_delta):
 	var old_position = $Player.position 
-	if Input.is_action_pressed("key_left") or $btn_left.pressed: $Player.position.x += -2.5
-	elif Input.is_action_pressed("key_right") or $btn_right.pressed: $Player.position.x += +2.5
+	if Input.is_action_pressed("key_left") or $btn_left.pressed: $Player.position.x += -256*_delta
+	elif Input.is_action_pressed("key_right") or $btn_right.pressed: $Player.position.x += +256*_delta
 	if old_position.x != $Player.position.x: $Player.animation = "walk"
+	elif Input.is_action_pressed("key_space") or $btn_fire.pressed: $Player.animation = "fire"
 	else: $Player.animation = "idle"
 	$Player.flip_h = bool($Player.position.x-old_position.x < 0)
 	if $Player.position.x < -4 : $Player.position.x = 48
 	elif $Player.position.x > 48: $Player.position.x = -4
 	
-func update_bullet():
-	if (Input.is_action_pressed("key_space") or $btn_fire.pressed) and (frame%10 == 0):
-		bullets.append($Bullet.duplicate())
-		bullets[-1].position = Vector2($Player.position.x, $Player.position.y)
-		bullets[-1].show()
-		add_child(bullets[-1])
+func update_bullet(_delta):
+	if (Input.is_action_pressed("key_space") or $btn_fire.pressed):
+		timer += _delta
+		if (timer > timer_limit):
+			timer -= timer_limit
+			bullets.append($Bullet.duplicate())
+			bullets[-1].position = Vector2($Player.position.x, $Player.position.y)
+			bullets[-1].show()
+			add_child(bullets[-1])
 	for n in bullets:
 		n.play()
 		if n.position.y < 8:
 			bullets.erase(n)
 			n.queue_free()
-		n.position = Vector2(n.position.x, n.position.y-2.5)
+		n.position = Vector2(n.position.x, n.position.y-128*_delta)
