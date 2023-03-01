247fb5b5ece1a7dda62fd79e19c881730bfc019c
whmsft <whmsft@outlook.com>
2023-03-01 13:10:18 +0530

    change Button objects for multi-touch

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,7 +2,7 @@
 ## Name: vechten   ##
 ## Author: @whmsft ##
 ## version: v0b1   ##
-## commit: 12      ##
+## commit: 14      ##
 #####################
 
 extends Node2D
@@ -26,24 +26,24 @@ func _ready():
 
 func _process(_delta):
 	FPS = Engine.get_frames_per_second()
-	$Label.text = "FPS:"+str(FPS)
+	$FPS_label.text = "FPS:"+str(FPS)
 	MOUSE = get_global_mouse_position()
 	update_player(_delta)
 	update_bullet(_delta)
 
 func update_player(_delta):
 	var old_position = $Player.position 
-	if Input.is_action_pressed("key_left") or $btn_left.pressed: $Player.position.x += -192*_delta
-	elif Input.is_action_pressed("key_right") or $btn_right.pressed: $Player.position.x += +192*_delta
+	if Input.is_action_pressed("key_left") or $btn_left.is_pressed(): $Player.position.x += -192*_delta
+	elif Input.is_action_pressed("key_right") or $btn_right.is_pressed(): $Player.position.x += +192*_delta
 	if old_position.x != $Player.position.x: $Player.animation = "walk"
-	elif Input.is_action_pressed("key_space") or $btn_fire.pressed: $Player.animation = "fire"
+	elif Input.is_action_pressed("key_space") or $btn_fire.is_pressed(): $Player.animation = "fire"
 	else: $Player.animation = "idle"
 	$Player.flip_h = bool($Player.position.x-old_position.x < 0)
 	if $Player.position.x < -4 : $Player.position.x = 48
 	elif $Player.position.x > 48: $Player.position.x = -4
 	
 func update_bullet(_delta):
-	if (Input.is_action_pressed("key_space") or $btn_fire.pressed):
+	if (Input.is_action_pressed("key_space") or $btn_fire.is_pressed()):
 		timer += _delta
 		if (timer > timer_limit):
 			timer -= timer_limit
